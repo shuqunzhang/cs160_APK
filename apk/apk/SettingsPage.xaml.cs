9945bdcd40302db4f08c1398af27a8e8f18c1694
Arturo Wu Zhou <aweezy90@gmail.com>
2012-04-07 18:48:41 -0700

    made SettingsPage look more like HomePage

--- apk/apk/SettingsPage.xaml.cs
+++ apk/apk/SettingsPage.xaml.cs
@@ -45,57 +45,117 @@ namespace apk
         const int skeletonCount = 6;
         Skeleton[] allSkeletons = new Skeleton[skeletonCount];
 
+        private Dictionary<JointType, SkeletonPoint> currentPos = new Dictionary<JointType, SkeletonPoint>() {
+            {JointType.HandRight, new SkeletonPoint()},
+            {JointType.HandLeft, new SkeletonPoint()},
+        };
+        private Dictionary<JointType, SkeletonPoint> lastPos = new Dictionary<JointType, SkeletonPoint>() {
+            {JointType.HandRight, new SkeletonPoint()},
+            {JointType.HandLeft, new SkeletonPoint()},
+        };
+        private Dictionary<JointType, SkeletonPoint> lastKeyPos = new Dictionary<JointType, SkeletonPoint>() {
+            {JointType.HandRight, new SkeletonPoint()},
+            {JointType.HandLeft, new SkeletonPoint()},
+        };
+        private DateTime lastKeyTime;
+        private enum Gesture : int { none, left_push };
+        private Gesture currentTrack = Gesture.none;
+        private int TOLERANCE = 20;
+        private int MINDIST = 5;
+
+        #region button helpers
+        private bool is_within(SkeletonPoint dip, double top, double left, double height, double width)
+        {
+            return top <= dip.Y && dip.Y <= (top + height) && left <= dip.X && dip.X <= (left + width);
+        }
+        private bool on_button(Button b, SkeletonPoint dip)
+        {
+            return is_within(dip, Canvas.GetTop(b), Canvas.GetLeft(b), b.Height, b.Width);
+        }
+        private Button select_button(SkeletonPoint dip)
+        {
+            if (on_button(button1, dip)) return button1;
+            if (on_button(button2, dip)) return button2;
+            if (on_button(button3, dip)) return button3;
+            if (on_button(button4, dip)) return button4;
+            return null;
+        }
+        private void button_click(Button b)
+        {
+            if (b == motionRange)
+            {
+                
+            }
+            else if (b == tabooGestures)
+            {
+                TabooGestures tg = new TabooGestures(kinectSensorChooser1);
+                this.NavigationService.Navigate(tg);
+            }
+            else if (b == tabooWords)
+            {
+
+            }
+            else if (b == volume)
+            {
+
+            }
+            else if (b == home)
+            {
+                HomePage h = new HomePage(kinectSensorChooser1);
+                this.NavigationService.Navigate(h);
+            }
+
+        }
+        //private void button3_Click(object sender, RoutedEventArgs e)
+        //{
+        //    TabooGestures tabboGesture = new TabooGestures();
+        //    this.NavigationService.Navigate(tabboGesture);
+        //}
+        #endregion button helpers
+
+        #region gesture helpers
+        private bool is_push(JointType jt)
+        {
+            return currentPos[jt].Z < lastPos[jt].Z
+                && Math.Abs(currentPos[jt].X - lastPos[jt].X) < TOLERANCE * 2
+                && Math.Abs(currentPos[jt].Y - lastPos[jt].Y) < TOLERANCE * 2;
+        }
+        private bool is_swipe_left(JointType jt)
+        {
+            return currentPos[jt].X < lastPos[jt].X - MINDIST
+                && Math.Abs(currentPos[JointType.HandRight].Y - lastPos[JointType.HandRight].Y) < TOLERANCE;
+        }
+        private bool is_swipe_up(JointType jt)
+        {
+            return currentPos[jt].Y < lastPos[jt].Y - MINDIST
+                && Math.Abs(currentPos[JointType.HandRight].X - lastPos[JointType.HandRight].X) < TOLERANCE;
+        }
+        private bool is_swipe_down(JointType jt)
+        {
+            return currentPos[jt].Y > lastPos[jt].Y + MINDIST
+                && Math.Abs(currentPos[JointType.HandRight].X - lastPos[JointType.HandRight].X) < TOLERANCE;
+        }
+        private bool isStill(JointType jt)
+        {
+            return Math.Abs(currentPos[jt].X - lastPos[jt].X) < TOLERANCE
+                && Math.Abs(currentPos[jt].Y - lastPos[jt].Y) < TOLERANCE
+                && Math.Abs(currentPos[jt].Z - lastPos[jt].Z) < TOLERANCE;
+        }
+        #endregion gesture helpers
+
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            //kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
             kinectSensorChooser1.Kinect.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(sensor_AllFramesReady);
         }
 
-
-        //void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
-        //{
-        //    KinectSensor old = (KinectSensor)e.OldValue;
-
-        //    StopKinect(old);
-
-        //    KinectSensor sensor = (KinectSensor)e.NewValue;
-
-        //    if (sensor == null)
-        //    {
-        //        return;
-        //    }
-
-
-
-
-        //    var parameters = new TransformSmoothParameters
-        //    {
-        //        Smoothing = 0.3f,
-        //        Correction = 0.0f,
-        //        Prediction = 0.0f,
-        //        JitterRadius = 1.0f,
-        //        MaxDeviationRadius = 0.5f
-        //    };
-        //    sensor.SkeletonStream.Enable(parameters);
-
-        //    sensor.SkeletonStream.Enable();
-
-        //    sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(sensor_AllFramesReady);
-        //    sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-        //    sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
-
-        //    try
-        //    {
-        //        sensor.Start();
-        //    }
-        //    catch (System.IO.IOException)
-        //    {
-        //        kinectSensorChooser1.AppConflictOccurred();
-        //    }
-        //}
-
         void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
         {
+            if (e == null) 
+            {
+                return;
+            }
+            #region from Home Page
             if (closing)
             {
                 return;
@@ -109,12 +169,75 @@ namespace apk
                 return;
             }
 
-            //set scaled position
-            //ScalePosition(headImage, first.Joints[JointType.Head]);
-            //ScalePosition(leftEllipse, first.Joints[JointType.HandLeft]);
-            //ScalePosition(rightEllipse, first.Joints[JointType.HandRight]);
 
-            GetCameraPoint(first, e);
+            //set scaled position
+            ScalePosition(cursor, first.Joints[JointType.HandRight]);
+            ScalePosition(nothing, first.Joints[JointType.HandLeft]);
+
+            //GetCameraPoint(first, e);
+
+            //LOGIC GOES HERE
+            //currentPos will be set in GetCameraPoint / ScalePosition
+
+            TimeSpan timeDifference = DateTime.Now - lastKeyTime;
+
+            //not important, I just didn't want to keep typing JointType.blah over and over
+            JointType right = JointType.HandRight;
+            JointType left = JointType.HandLeft;
+
+            //debugging
+            //debug.Content = "";
+
+            //for styling buttons on hover,etc
+            motionRange.Tag = "";
+            tabooGestures.Tag = "";
+            tabooWords.Tag = "";
+            volume.Tag = "";
+            home.Tag = "";
+
+            //if (on_button(button4, currentPos[right]))
+            //{ //hovering button4
+            //    button4.Tag = "hover";
+            //}
+
+            bool resetTrack = true;
+            switch (currentTrack)
+            {
+                case Gesture.none:
+                    resetTrack = false;
+                    if (is_push(left))
+                    { //push w/ left hand
+                        currentTrack = Gesture.left_push;
+                        lastKeyTime = DateTime.Now;
+                        lastKeyPos[JointType.HandLeft] = currentPos[JointType.HandLeft];
+                    }
+                    break;
+
+                case Gesture.left_push:
+                    if (is_push(left))
+                    {
+                        resetTrack = false;
+                        if (lastKeyPos[left].Z - currentPos[left].Z > 0.15)
+                        {
+                            Button b = select_button(currentPos[right]);
+                            if (b != null)
+                            {
+                                b.Tag = "press";
+                                if (b.IsEnabled) button_click(b);
+                                resetTrack = true;
+                            }
+                        }
+                    }
+                    break;
+            }
+            if (resetTrack)
+            {
+                currentTrack = Gesture.none;
+                lastKeyTime = DateTime.Now;
+            }
+            lastPos[JointType.HandRight] = currentPos[JointType.HandRight];
+            lastPos[JointType.HandLeft] = currentPos[JointType.HandLeft];
+            #endregion from Home Page
 
         }
 
@@ -229,19 +352,9 @@ namespace apk
 
         }
 
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            closing = true;
-            StopKinect(kinectSensorChooser1.Kinect);
-        }
         #endregion SampleCode
 
-        private void button3_Click(object sender, RoutedEventArgs e)
-        {
-            TabooGestures tabboGesture = new TabooGestures();
-            this.NavigationService.Navigate(tabboGesture);
-        }
+        
 
     }
 }
